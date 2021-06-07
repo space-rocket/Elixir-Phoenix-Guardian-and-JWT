4b9476d7a956881b80994bff0391829878074cdc
admin <bigbassroller@gmail.com>
2021-06-06 23:32:34 -0700

    login controller tests passing

---
+++ lib/my_app_web/controllers/json_api/user_login_controller.ex
@@ -0,0 +1,29 @@
+defmodule MyAppWeb.JsonApi.UserLoginController do
+  use MyAppWeb, :controller
+
+  alias MyApp.Accounts
+  alias MyApp.Accounts.User
+  # alias MyAppWeb.UserAuth
+  alias MyApp.Guardian
+
+
+  def create(conn, %{"email" => nil}) do
+    conn
+    |> put_status(401)
+    |> render("error.json", message: "User could not be authenticated")
+  end
+
+  def create(conn, %{"email" => email, "password" => password}) do
+    case Accounts.get_user_by_email_and_password(email, password) do
+      %User{} = user ->
+        {:ok, jwt, _full_claims} = Guardian.encode_and_sign(user, %{})
+
+        conn
+        |> render("create.json", user: user, jwt: jwt)
+      nil ->
+        conn
+        |> put_status(401)
+        |> render("error.json", message: "User could not be authenticated")
+    end
+  end
+end
---
+++ test/my_app_web/controllers/json_api/user_login_controller_test.exs
@@ -0,0 +1,39 @@
+defmodule MyAppWeb.JsonApi.UserLoginControllerTest do
+  use MyAppWeb.ConnCase, async: true
+
+  import MyApp.AccountsFixtures
+
+  setup do
+    %{user: user_fixture()}
+  end
+
+  describe "POST /api/session" do
+    test "with no credentials user can't login", %{conn: conn} do
+      conn = post(conn, Routes.json_api_user_login_path(conn, :create), email: nil, password: nil)
+      assert %{"message" => "User could not be authenticated"} = json_response(conn, 401)
+    end
+
+    test "with invalid password user cant login", %{conn: conn, user: user} do
+      conn =
+        post(conn, Routes.json_api_user_login_path(conn, :create),
+          email: user.email,
+          password: "wrongpass"
+        )
+
+      assert %{"message" => "User could not be authenticated"} = json_response(conn, 401)
+    end
+
+    test "with valid password user can login", %{conn: conn, user: user} do
+      conn =
+        post(conn, Routes.json_api_user_login_path(conn, :create),
+          email: user.email,
+          password: valid_user_password()
+        )
+
+      assert %{
+        "data" => %{"token" => "" <> _},
+        "message" => "You are successfully logged in" <> _
+      } = json_response(conn, 200)
+    end
+  end
+end
